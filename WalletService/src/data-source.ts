5c4e07534dc5a92c01e53377b1328dd65ead5014
stevesdiary <stephenoyeyemi@gmail.com>
2024-06-25 12:04:09 +0100

    Update data-source.ts
    Remove account entity

--- WalletService/src/data-source.ts
+++ WalletService/src/data-source.ts
@@ -2,7 +2,6 @@ import "reflect-metadata"
 import * as dotenv from "dotenv";
 import { DataSource } from "typeorm"
 import { User } from "./entity/User";
-import { Account } from "./entity/Account";
 import { Transaction } from "./entity/Transaction";
 dotenv.config();
 
@@ -15,7 +14,7 @@ export const AppDataSource = new DataSource({
   database: process.env.DATABASE,
   synchronize: true,
   logging: false,
-  entities: [User, Account, Transaction],
+  entities: [User, Transaction],
   migrations: [],
   subscribers: [],
 })
