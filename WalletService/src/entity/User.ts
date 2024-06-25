0e8f16732e587d199d000a90ed3e5e409d661fea
stevesdiary <stephenoyeyemi@gmail.com>
2024-06-25 12:04:57 +0100

    Create User.ts
    Create user entity

---
+++ WalletService/src/entity/User.ts
@@ -0,0 +1,38 @@
+import { IsAlphanumeric, IsEmail, IsNotEmpty, IsNumber, IsString } from "class-validator";
+import { Entity, PrimaryGeneratedColumn, Column, OneToOne, JoinColumn, Unique } from "typeorm";
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string
+
+  @Column()
+  @IsString()
+  @IsNotEmpty()
+  firstName: string
+
+  @Column()
+  @IsEmail()
+  @IsNotEmpty()
+  email: string
+
+  @Column()
+  @IsString()
+  @IsNotEmpty()
+  userType: string
+
+  @Column()
+  @IsAlphanumeric()
+  password: string
+
+  @Column()
+  @IsNumber()
+  balance: number
+
+  @Column()
+  @IsNotEmpty()
+  accountNumber: string
+  Unique: true;
+  userRepository: any;
+
+}
