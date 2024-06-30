0d7353398d242599bb09e241cc493d59f5802049
stevesdiary <stephenoyeyemi@gmail.com>
2024-06-30 22:05:21 +0100

    Correction

---
+++ src/controller/loginController.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from "express";
+import { AppDataSource } from "../data-source";
+import { User } from "../entity/User";
+
+const userRepository  = AppDataSource.manager.getRepository(User);
+const loginController = {
+  login: async (req: Request, res: Response) => {
+    try {
+      const { email, password } = req.body;
+      const user = await userRepository.findOne( email );
+      if ( !user ) { 
+        return res.status( 200 ).send({message: "User not found"}); 
+      }
+      
+    } catch (error) {
+      
+    }
+  }
+  
+
+}
+
+export default loginController;
