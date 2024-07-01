d90a6c2fefbc22f115a64fdda8b908131f599027
stevesdiary <stephenoyeyemi@gmail.com>
2024-07-01 01:05:00 +0100

    Update transactionRoute.ts

--- src/route/transactionRoute.ts
+++ src/route/transactionRoute.ts
@@ -2,9 +2,9 @@ import { Router } from "express";
 const router = Router();
 import transactionController from "../controller/transactionController";
 
-router.post("/createtransaction", transactionController.create);
-router.get("/alltransactions", transactionController.getAll);
-router.delete("/deletetransaction/:id", transactionController.delete);
+router.post("/transaction/create", transactionController.create);
+router.get("/transactions/all", transactionController.getAll);
+router.delete("/transaction/:id", transactionController.delete);
 router.get("/transaction/:id", transactionController.getOne);
 router.post("/transaction/transfer", transactionController.transferFunds);
 router.post("/transaction/fund", transactionController.fundAccount);
