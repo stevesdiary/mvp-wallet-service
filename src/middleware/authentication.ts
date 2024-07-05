5cc0ede03a022fdfe9df5ea0222ab711e8b334cc
Stephen Oyeyemi <stephenoyeyemi@gmail.com>
2024-07-05 23:25:55 +0100

    Create authentication.ts

---
+++ src/middleware/authentication.ts
@@ -0,0 +1,46 @@
+import { Response, Request, NextFunction } from "express";
+import * as jwt from 'jsonwebtoken';
+const secret: string = process.env.SECRETKEY || "Secret_key"
+
+export const authentication = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const header = req.headers['authorization']
+      let token = req.headers.authorization;
+      token = header?.split(' ')[1];
+      // const decoded = jwt.verify(token, secret) as jwt.JwtPayload;
+      // req.user = decoded;
+      if(token == null || token.length === 0 ) {
+        return res.status(403).json({message: 'Invalid or expired token'})
+      }
+      next();
+    } catch (error) {
+      console.log(error)
+      return res.status(500).json({message: 'Error occured', error})
+    }
+}
+
+export const verifyUser = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const verifyUser = (...allowedType:string[]) => {
+        const header = req.headers['authorization'];
+      let token: string = req.headers.authorization || '';
+      token = header?.split(' ')[1] || 'STRING';
+
+      if (!token) {
+        return res.status(401).json({ message: 'No token provided' });
+      }
+      const decoded = jwt.verify(token, secret)as jwt.JwtPayload;
+      
+      let userType = decoded.userType;
+      console.log("User", userType);
+      if (allowedType.includes(userType)) {
+        next();
+      }
+      else {
+        return res.status(403).json({ message: 'Access denied: Insufficient rights' });
+      }
+    }
+    } catch (error) {
+      return res.status(500).json({message: 'An error occurred', error})
+    }
+  }
