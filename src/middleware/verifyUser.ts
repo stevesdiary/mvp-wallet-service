63d6f5f7c3aa72575a9525e3b233b35ea6b8649e
Stephen Oyeyemi <stephenoyeyemi@gmail.com>
2024-07-12 21:40:53 +0100

    Update verifyUser.ts

--- src/middleware/verifyUser.ts
+++ src/middleware/verifyUser.ts
@@ -1,23 +1,24 @@
-import { Request, Response, NextFunction } from "express";
+// import { Request, Response, NextFunction } from "express";
 
-export const verifyUser = (...allowedTypes: string[]) => {
-  return (req: Request, res: Response, next: NextFunction) => {
-    try {
-      if (!req.user) {
-        console.error('Unauthorized');
-        return res.status(401).json({ message: 'Unauthorized' });
-      }
+// export const verifyUser = (...allowedTypes: string[]) => {
+//   return (req: Request, res: Response, next: NextFunction) => {
+//     try {
+//       const user = req.header.
+//       if (!req.user) {
+//         console.error('Unauthorized');
+//         return res.status(401).json({ message: 'Unauthorized' });
+//       }
 
-      const userType = req.user.userType;
-      if (allowedTypes.includes(userType)) {
-        next();
-      } else {
-        console.error('Access forbidden: Insufficient rights');
-        return res.status(403).json({ message: 'Access forbidden: Insufficient rights' });
-      }
-    } catch (error) {
-      console.error('An error occurred', error);
-      return res.status(500).json({ message: 'An error occurred', error });
-    }
-  };
-};
+//       const userType = req.user.userType;
+//       if (allowedTypes.includes(userType)) {
+//         next();
+//       } else {
+//         console.error('Access forbidden: Insufficient rights');
+//         return res.status(403).json({ message: 'Access forbidden: Insufficient rights' });
+//       }
+//     } catch (error) {
+//       console.error('An error occurred', error);
+//       return res.status(500).json({ message: 'An error occurred', error });
+//     }
+//   };
+// };
