53d3f026ab8a439aa848b1637c6f868d17e7f1f1
Stephen Oyeyemi <stephenoyeyemi@gmail.com>
2024-07-12 22:17:00 +0100

    Update transactionRoute.ts
    Change get to post request for withdrawal

--- src/route/transactionRoute.ts
+++ src/route/transactionRoute.ts
@@ -8,5 +8,5 @@ router.delete("/transaction/:id", transactionController.delete);
 router.get("/transaction/:id", transactionController.getOne);
 router.post("/transaction/transfer", transactionController.transferFunds);
 router.post("/transaction/fund", transactionController.fundAccount);
-router.get("/transaction/withdraw", transactionController.withdraw);
+router.post("/transaction/withdraw", transactionController.withdraw);
 export default router;
