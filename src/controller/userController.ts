b061153cd666441651bd298fe800d0163db624e4
Stephen Oyeyemi <stephenoyeyemi@gmail.com>
2024-07-19 23:15:46 +0100

    Update userController.ts

--- src/controller/userController.ts
+++ src/controller/userController.ts
@@ -18,7 +18,7 @@ const userController = {
       return res.status(200).json({ message: "Got one!", user })
     }
     catch (error) {
-      console.log(error);
+      return res.status(500).send({ message: 'An error occured', error})
     }
   },
 
@@ -133,7 +133,6 @@ const userController = {
       await userRepository.save(newUser);
       return res.status(201).json({ message: 'User created', user })
     } catch (error) {
-      console.log(error)
       return res.status(500).json({ message: "Something went wrong!", error })
     }
   },
@@ -143,7 +142,6 @@ const userController = {
       const allUser = await userRepository.find()
       return res.status(200).json({ message: 'Got users', allUser })
     } catch (error) {
-      console.log(error)
       return res.status(500).json({ message: 'Error occured', error })
     }
   },
@@ -175,7 +173,6 @@ const userController = {
       await userRepository.save(user)
       return res.status(200).json({ message: "User data updated", user})
     } catch (error) {
-      console.log("Error occured", error)
       return res.status(500).json({ message: "Error occured", error })
     }
   },
@@ -187,7 +184,6 @@ const userController = {
       if (!deleteUser) { return res.status(404).json({ message: 'User record not deleted, an error must have occured' }) }
       return res.status(200).json({ message: 'User record deleted successfully' })
     } catch (error) {
-      console.log(error)
       return res.status(500).json({ message: 'Error occured', error })
     }
   }
