d9be2035486b40175119d0e5c55db89daeac8cb5
steve-aella <stephenoyeyemi@gmail.com>
2024-09-08 00:02:43 +0100

    Update loginRoute.ts

--- src/route/loginRoute.ts
+++ src/route/loginRoute.ts
@@ -2,6 +2,6 @@ import { Router } from "express";
 const router = Router();
 import loginController from "../controller/loginController";
 
-router.get("/login", loginController.login)
+router.get("/login", loginController.login);
 
 export default router;
