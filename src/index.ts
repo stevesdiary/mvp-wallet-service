dc23667b860fdf038804b8838cfb11cae8ef2c57
steve-aella <stephenoyeyemi@gmail.com>
2024-09-28 20:19:59 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ require("dotenv").config();
 import "reflect-metadata";
 import * as bodyParser from "body-parser";
 import { AppDataSource } from "./data-source";
-import {Request, Response} from "express";
+import { Request, Response } from "express";
 import express from "express";
 import userRoute from "./route/userRoute";
 import accountRoute from './route/loginRoute';
@@ -26,7 +26,7 @@ const port = process.env.LOCAL_PORT || 8888;
 AppDataSource.initialize().then(async () => {
 	app.listen(port, () => {
 		console.log("Database connected successfully!");
-		console.log(`App running on port ${port}.`);
+		console.log(`App running on port ${ port }.`);
 	});
 }).catch(error => {
 	console.error("Database connection error:", error);
