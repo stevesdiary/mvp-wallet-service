c566d65c3de7880989c910e0548d1c04701edc06
Stephen Oyeyemi <stephenoyeyemi@gmail.com>
2024-12-13 23:55:12 +0100

    Update loginRoute.ts
    Add logout route

--- src/route/loginRoute.ts
+++ src/route/loginRoute.ts
@@ -2,6 +2,6 @@ import { Router } from "express";
 const router = Router();
 import loginController from "../controller/loginController";
 
-router.get("/login", loginController.login);
-
+router.post("/login", loginController.login);
+router.post("/logout", login Controller.logout);
 export default router;
