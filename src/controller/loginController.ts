9da3038f7f269e436e182fea343c55032d6a83ed
Stephen Oyeyemi <stephenoyeyemi@gmail.com>
2024-12-21 09:58:46 +0100

    Update loginController.ts

--- src/controller/loginController.ts
+++ src/controller/loginController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { AppDataSource } from "../data-source";
 import { User } from "../entity/User";
+import bcrypt from 'bcrypt.js';
 
 const userRepository  = AppDataSource.manager.getRepository(User);
 const loginController = {
@@ -8,11 +9,16 @@ const loginController = {
     try {
       const { email, password } = req.body;
       const user = await userRepository.findOne( email );
+      const match = await bcrypt.compare(password, user.password);
       if ( !user ) { 
         return res.status( 200 ).send({message: "User not found"}); 
       }
-      
+      if (!match) {
+        return res.status(400).send({ message: 'Password is not correct! Enter a valid password'};
+      }
+       
     } catch (error) {
+      console.log(error);
       return res.status(500).send( {message: 'an error ocurred', error} )
     }
   }
